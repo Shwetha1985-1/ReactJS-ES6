9a74890a62ed3ce0f2536311317c8871ab8b103b
Shwetha1985-1 <82023397+Shwetha1985-1@users.noreply.github.com>
2024-04-29 22:14:22 +0530

    Add files via upload
    student profile complex object

---
+++ StudentProfile.js
@@ -0,0 +1,16 @@
+const studentProfile={
+    firstName:"Shwetha",
+    lastName:"Patil",
+    class:{name:"Introduction to JavaScript",
+           teacher:{name:"Sharada P",
+                    email:"sp@gmail.com"}},
+    grades:{maths:"A",
+    science:"A+",Social:"C"}
+
+}
+let full_name=studentProfile.firstName+" "+studentProfile.lastName;
+console.log("Student Full Name is:"+full_name);
+console.log("Class Name is:"+studentProfile.class["name"]);//Print the class name using bracket notation
+console.log("Teacher's email is:"+studentProfile.class.teacher["email"]); 
+studentProfile.grades["maths"]='A+'; // Change the grade for maths
+console.log("updated grade is:"+studentProfile.grades["maths"]); //upated grade
